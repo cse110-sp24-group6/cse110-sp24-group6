c74facd23f737aa5a089a2e9786d187ad3c5c90c
Yashil Vora <yvora@ucsd.edu>
2024-05-23 13:35:36 -0700

    stylistic changes

--- source/assets/scripts/dailylog.js
+++ source/assets/scripts/dailylog.js
@@ -5,7 +5,7 @@ document.addEventListener('DOMContentLoaded', function () {
         "January", "February", "March", "April", "May", "June",
         "July", "August", "September", "October", "November", "December"
     ];
-    const daysOfWeek = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
+    const daysOfWeek = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
     let currentDate = new Date();
     let logs = {};
 
